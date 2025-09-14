f5b059dd9d96e6abcddf9e24e549e25f4a0a015e
Linoy Furian <linoy.f197@gmail.com>
2025-09-14 12:34:54 +0300

    adding scroll pane

--- ui-fx/src/fx/app/MainApp.java
+++ ui-fx/src/fx/app/MainApp.java
@@ -4,6 +4,7 @@ import fx.system.SEmulatorSystemController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
@@ -14,13 +15,10 @@ public class MainApp extends Application {
         primaryStage.setTitle("S-Emulator");
 
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/fx/system/semulatorSystem.fxml"));
-        BorderPane root = loader.load();                       // טוענים דרך ה-loader
+        ScrollPane root = loader.load();
         SEmulatorSystemController system = loader.getController();
 
-        // אם צריך להזרים תלותים לתוך ה-root (לא חובה אם ה-root יוצר engine בעצמו)
-        // system.setEngine(new SEmulatorEngineImpl());
-
-        Scene scene = new Scene(root, 1100, 600);
+        Scene scene = new Scene(root, 1100, 680);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
